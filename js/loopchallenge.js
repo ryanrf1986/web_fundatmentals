dae3fe2001ea6d74f474076b71e1ab833e046e1b
ryanrf1986 <ryanfleming@ymail.com>
2022-03-28 10:47:33 -0700

    confirm

--- js/loopchallenge.js
+++ js/loopchallenge.js
@@ -1,3 +1,4 @@
+// 1.
 function printoddsnumtonum(startNum, endNum) {
     for (var i = startNum; i <= endNum; i++) {
       if (i % 2 == 1) {
@@ -5,8 +6,10 @@ function printoddsnumtonum(startNum, endNum) {
       }
     }
   }
-  printoddsnumtonum(1, 20);
+printoddsnumtonum(1, 20);
 
+
+// 2.
 function printmultnumtonum(startNum, endNum){
     for (var i= startNum; i >= endNum; i--){
         if ( i % 3 ===0){
@@ -15,4 +18,29 @@ function printmultnumtonum(startNum, endNum){
     }
 }
 printmultnumtonum(100, 0);
+
+
+// 3.
+function printsequence (startNum, endNum){
+  for (var i = startNum; i >= endNum; i -= 1.5)
+    console.log(i);
+}
+printsequence(4, -3.5)
+
+
+// 4.
+let sum = 0;
+  for( var i = 1; i <= 100; i++){
+    sum = sum + i;
+}
+console.log(sum);
+
+
+// 5.
+let product = 1;
+  for( var i = 1; i<=12; i++){
+    product *= i;
+  }
+console.log(product);
+  
 
