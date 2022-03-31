d8a82e730beb735a27e6992da61943ad3cb876c7
ryanrf1986 <ryanfleming@ymail.com>
2022-03-30 19:13:44 -0700

    commit

---
+++ belt_exam/main.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    alert("Your Cart is empty!");
+}
+
+document.querySelector('.link').addEventListener('click', () => document.querySelector('#bar').style = "display:none")
