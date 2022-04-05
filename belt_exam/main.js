47c4d9e2c387bd9093fba7c8d88b757d7ad4502e
ryanrf1986 <101135776+ryanrf1986@users.noreply.github.com>
2022-04-04 17:06:23 -0700

    Delete belt_exam directory
    /

--- belt_exam/main.js
+++
@@ -1,5 +0,0 @@
-function showMessage() {
-    alert("Your Cart is empty!");
-}
-
-document.querySelector('.link').addEventListener('click', () => document.querySelector('#bar').style = "display:none")
